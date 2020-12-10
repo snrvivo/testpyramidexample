c2afa660859b66fcf3af2a05b38e880a1d92a591
Senor Vivo <snrvivo@gmail.com>
2020-12-10 20:22:33 +0000

    Test trigger 2

--- rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxDemoServerApplication.java
+++ rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxDemoServerApplication.java
@@ -1,7 +1,7 @@
 package rxdemo.endpoints;
 
 import spark.servlet.SparkApplication;
-
+//test build 2
 public class RxDemoServerApplication implements SparkApplication {
     @Override
     public void init() {
